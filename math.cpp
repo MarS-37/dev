b84d0c59c64345fd8f55ed44f6db4de73836234e
Mars-37 <master_sbo@mail.ru>
2024-04-28 11:39:16 +0300

    lesson six

--- math.cpp
+++ math.cpp
@@ -1,7 +1,10 @@
-#if 1 //(or if true)
+const char* Log(const char* message)
+{
+	return message;
+}
+
 int Multiply(int a, int b)
 {
-	int result = a * b;
-	return result;
+	Log("Multiply");
+	return a * b;
 }
-#endif

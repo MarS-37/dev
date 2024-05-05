03ac676df6bb6e05ebb6d0983767eeba7bc5f9f2
Mars-37 <master_sbo@mail.ru>
2024-05-05 20:35:53 +0300

    less 029

--- main.cpp
+++ main.cpp
@@ -2,66 +2,38 @@
 #include <string>
 
 
-class Printable
+class Entity
 {
+//private:
+//protected:
 public:
-	virtual std::string GetClassName() = 0;
-};
+	int X, Y;
 
+	void Print();
 
-class Entity : public Printable
-{
 public:
-	virtual std::string GetName() { return "Entity"; }
-	std::string GetClassName() override { return "Entity"; }
+	Entity() 
+	{
+		X = 0;
+		Print();
+	}
 };
 
 
 class Player : public Entity
 {
-private:
-	std::string m_Name;
-
 public:
-	Player(const std::string& name)
-		: m_Name(name){}
-
-	std::string GetName() override { return m_Name; }
-	std::string GetClassName() override { return "Player"; }
+	Player()
+	{
+		X = 2;
+		Print();
+	}
 };
-
-
-void PrintName(Entity* entity)
-{
-	std::cout << entity->GetName() << std::endl;
-}
-
-
-class A : public Printable
-{
-public:
-	std::string GetClassName() override { return "A"; }
-};
-
-
-void Print(Printable* obj)
-{
-	std::cout << obj->GetClassName() << std::endl;
-}
 
 
 int main()
 {
-	Entity* e = new Entity();
-	//PrintName(e);
-
-	Player* p = new Player("Cherno");
-	//PrintName(p);
-
-	Print(e);
-	Print(p);
-	Print(new A());  // для примера
-
-	delete e;
-	delete p;
+	Entity e;
+	e.Print();
+	e.X = 3;
 }

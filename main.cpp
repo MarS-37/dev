da2c2f44f2436498655bb10a32a641d4bd012fdf
Mars-37 <master_sbo@mail.ru>
2024-05-05 14:09:39 +0300

    less 020

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 
-void Log(const char* message);
+class  Log
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError) {
+			std::cout << "[ERROR]: " << message << std::endl;
+		}
+	}
+	void Warning(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning) {
+			std::cout << "[WARNING]: " << message << std::endl;
+		}
+	}
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo) {
+			std::cout << "[INFO]: " << message << std::endl;
+		}
+	}
+
+private:
+	int m_LogLevel = LogLevelInfo;
+};
 
 int main()
 {
-	Log("Hello World!");
-	std::cin.get();
+	Log log;
+	log.SetLevel(log.LogLevelInfo);
+	log.Error("Error messages");
+	log.Warning("Warning messages");
+	log.Info("Info messages");
 }

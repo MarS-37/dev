e9e5f9fffb155a113de28ba818fc78edec042575
Mars-37 <master_sbo@mail.ru>
2024-05-07 22:07:23 +0300

    less 031

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 #include <string>
 
-
-void PrintString(std::string& string)
-{
-	std::cout << string << std::endl;
-}
+#include <stdlib.h>
 
 int main()
 {
-	std::string name = std::string("Cherno") + " hello";
-	bool contains = name.find("no_") != std::string::npos;
+	using namespace std::string_literals;
 
-	PrintString(name);
 
-	std::cout << name << std::endl;
-	std::cout << contains << std::endl;
+	std::string name0 = std::string("Cherno") + " hello";
+
+	std::string name1 = "Cherno"s + " hello"; // C++14
+	std::string name2 = u8"Cherno"s + " hello"; // C++14
+	std::wstring name3 = L"Cherno"s + L" hello"; // C++14
+	std::u32string name4 = U"Cherno"s + U" hello"; // C++14
+
+	const char* name5 = u8"Cherno";
+	const wchar_t* name6 = L"Cherno";
+
+
+	const char16_t* name7 = u"Cherno";
+	const char32_t* name8 = U"Cherno";
+
+	const char* example1 = R"(Line1
+		Line2
+		Line3
+		Line4)";
+
+	const char* example2 = "Line1\n"
+		"Line2\n"
+		"Line3\n";
+
+
+	//std::cout << (L)name3 << std::endl;
 }

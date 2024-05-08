32fff8517115d8457781d8083af093366a51afef
Mars-37 <master_sbo@mail.ru>
2024-05-08 18:00:37 +0300

    less 035

--- main.cpp
+++ main.cpp
@@ -2,16 +2,32 @@
 #include <string>
 
 
+class Example
+{
+public:
+	Example()
+	{
+		std::cout << "Create Entity!" << std::endl;
+	}
+	Example(int x)
+	{
+		std::cout << "Create Entity with " << x << "!" << std::endl;
+	}
+};
+
+
 class Entity
 {
 private:
 	std::string m_Name;
-	int m_Score;
+	Example m_Example;
 
 public:
-	Entity()
-		: m_Score(0), m_Name("Unknown")
-	{}
+	Entity()		
+	{
+		m_Name = std::string("Unknown");
+		m_Example = Example(8);
+	}
 	Entity(const std::string& name)
 		: m_Name(name)
 	{		
@@ -26,8 +42,4 @@ public:
 int main()
 {
 	Entity e0;
-	std::cout << e0.GetName() << std::endl;
-
-	Entity e1("Cherno");
-	std::cout << e1.GetName() << std::endl;
 }

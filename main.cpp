be4450d8de1a06dbde6a534740b549533382b979
Mars-37 <master_sbo@mail.ru>
2024-05-08 16:43:35 +0300

    less 034

--- main.cpp
+++ main.cpp
@@ -1,59 +1,22 @@
 #include <iostream>
 #include <string>
 
-#include <stdlib.h>
-
 
 class Entity
 {
 private:
-	int* m_X;
+	std::string m_Name;
 
 public:
-	const int* const GetX() const
+	const std::string& GetName() const
 	{
-		return m_X;
-	}
-	void SetX(int x) const
-	{
-		m_X = x; // error
+		return m_Name;
 	}
 };
 
 
 int main()
 {
-	const int MAX_AGE = 90;
-
-
-	const int* a = new int;
-
-	*a = 2;		// error
-	a = (int*)&MAX_AGE;
-
-
-	int* const b = new int;
-
-	*b = 2;
-	b = (int*)&MAX_AGE; // error
-		
-
-	int const* c = new int;
-
-	*c = 2;		// error
-	c = (int*)&MAX_AGE;
-
-
-	const int* const d = new int;
-
-	*d = 2;		// error
-	d = (int*)&MAX_AGE; // error
-
-
-	std::cout << *a << std::endl;
-
-	delete a;
-	delete b;
-	delete c;
-	delete d;
+	const Entity e;
+	e.GetName();
 }

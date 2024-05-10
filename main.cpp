66348888b3f90db336e244d111a10c065269ae80
Mars-37 <master_sbo@mail.ru>
2024-05-10 17:42:08 +0300

    less 036

--- main.cpp
+++ main.cpp
@@ -8,14 +8,7 @@ static int s_Speed = 2;
 
 int main()
 {
-	s_Speed = s_Level > 5 ? 10 : 5;
-
-	std::string rank = s_Level > 10 ? "Master" : "Begginer";
-
-	std::string otherRank;
-
-	if (s_Level > 10)
-		otherRank = "Master";
-	else
-		otherRank = "Beginer";	
+	s_Speed = s_Level > 5 ? s_Level > 10 ? 15 : 10 : 5;
+	
+	std::cout << s_Speed << std::endl;
 }

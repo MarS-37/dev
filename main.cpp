838d0078029d2b8a1cbc1d860543553e5627d210
Mars-37 <master_sbo@mail.ru>
2024-05-11 12:04:32 +0300

    less 040 Operator Overloading

--- main.cpp
+++ main.cpp
@@ -13,12 +13,17 @@ struct Vector2
 {
 	float x, y;
 
-	Vector2(float x, y)
+	Vector2(float x, float y)
 		: x(x), y(y) {}
 
 	Vector2 Add(const Vector2& other) const
 	{
 		return Vector2(x + other.x, y + other.y);
+	}
+
+	Vector2 operator+(const Vector2& other) const
+	{
+		return Add(other);
 	}
 
 	Vector2 Multiply(const Vector2& other) const
@@ -34,5 +39,6 @@ int main()
 	Vector2 speed(0.5f, 1.5f);
 	Vector2 powerup(1.1f, 1.1f);
 
-	Vector2 result = position.Add(speed.Multiply(powerup));
+	Vector2 result1 = position.Add(speed.Multiply(powerup));
+	Vector2 result2 = position + speed; // * powerup));
 }

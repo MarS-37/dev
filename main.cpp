72e0912c99643fcee2a02557ad9d5e3edf4bd67b
Mars-37 <master_sbo@mail.ru>
2024-05-12 15:48:44 +0300

    less 048 Local Static

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|               Optimizing the usage of std::vector                |
+//|                            Local Static                          |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
@@ -8,47 +8,20 @@
 #include <iostream>
 #include <string>
 
-#include <vector>
 
-
-struct Vertex
+int Function()
 {
-	float X, Y, Z;
+	static int i = 0;
+	++i;
+	std::cout << i << std::endl;
 
-	Vertex(float x, float y, float z)
-		: X(x), Y(y), Z(z) {}
-
-	Vertex(const Vertex& vertex)
-		: X(vertex.X), Y(vertex.Y), Z(vertex.Z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
-};
-
-
-std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
-{
-	stream << vertex.X << ", " << vertex.Y << ", " << vertex.Z;
-
-	return stream;
-}
-
-
-void Function(const std::vector<Vertex>& vertices)
-{
-
+	return i;
 }
 
 
 int main()
 {
-	std::vector<Vertex> vertices;
-	vertices.reserve(3);
-	vertices.emplace_back(1, 2, 3);
-	vertices.emplace_back(4, 5, 6);
-	vertices.emplace_back(7, 8, 9);
-
-	for (Vertex v : vertices) {
-		std::cout << v << std::endl;
+	for (auto i = 0; i < 5; ++i) {
+		Function();
 	}
 }

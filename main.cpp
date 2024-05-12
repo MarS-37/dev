9be985c1c4a75e211f9872f1d94668c7476fbb9f
Mars-37 <master_sbo@mail.ru>
2024-05-12 19:54:12 +0300

    less 050 Using Dynamic Libraries

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-//+-----------------------------------------------------------------+
-//|                                                                 |
-//|                Using Libraries (Static Linking)                 |
-//| https://github.com/glfw/glfw/releases/download/3.4/glfw-3.4.zip |
-//|                                                                 |
-//+-----------------------------------------------------------------+
+//+------------------------------------------------------------------+
+//|                                                                  |
+//|                      Using Dynamic Libraries                     |
+//|                                                                  |
+//+------------------------------------------------------------------+
 
 
 #include <iostream>
-//#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 
-extern "C" int glfwInit();
 
 int main()
 {
 	int a = glfwInit();
 	std::cout << "GLFW WORK: " << a << std::endl;
+
+	std::cin.get();
 }

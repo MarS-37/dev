4cb832288ed54c8dfc6ecd27757d025d2eb53bb8
Mars-37 <master_sbo@mail.ru>
2024-05-12 13:29:32 +0300

    less 047 Optimizing the usage of std::vector

--- main.cpp
+++ main.cpp
@@ -43,10 +43,10 @@ void Function(const std::vector<Vertex>& vertices)
 int main()
 {
 	std::vector<Vertex> vertices;
-
-	vertices.push_back(Vertex(1, 2, 3));
-	vertices.push_back(Vertex(4, 5, 6));
-	vertices.push_back(Vertex(7, 8, 9));
+	vertices.reserve(3);
+	vertices.emplace_back(1, 2, 3);
+	vertices.emplace_back(4, 5, 6);
+	vertices.emplace_back(7, 8, 9);
 
 	for (Vertex v : vertices) {
 		std::cout << v << std::endl;

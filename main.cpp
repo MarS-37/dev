f4a4c1e81a86e96ad6c7a83109d7b161f62f73bf
Mars-37 <master_sbo@mail.ru>
2024-05-12 18:44:09 +0300

    less 049 Using Libraries (Static Linking)

--- main.cpp
+++ main.cpp
@@ -1,31 +1,18 @@
-//+------------------------------------------------------------------+
-//|                                                                  |
-//|                            Local Static                          |
-//|                                                                  |
-//+------------------------------------------------------------------+
+//+-----------------------------------------------------------------+
+//|                                                                 |
+//|                Using Libraries (Static Linking)                 |
+//| https://github.com/glfw/glfw/releases/download/3.4/glfw-3.4.zip |
+//|                                                                 |
+//+-----------------------------------------------------------------+
 
 
 #include <iostream>
 #include <string>
 
-
-class Singleton
-{
-private:
-	static Singleton& s_Instance;
-
-public: 
-	static Singleton& Get()
-	{
-		static Singleton instance;
-		return instance;
-	}
-
-	void Hello() {}
-};
+#include "GLFW/glfw3.h"
 
 
 int main()
 {
-	Singleton::Get().Hello();
+	
 }

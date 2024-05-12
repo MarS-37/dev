c5189da1bdfe415068c3771804659d25ba2497e2
Mars-37 <master_sbo@mail.ru>
2024-05-12 09:57:41 +0300

    less 045 Operator ->

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                   Copying and Copy Constructors                  |
+//|                             Operator ->                          |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
@@ -9,63 +9,26 @@
 #include <string>
 
 
-class String
+class Entity
 {
-private:
-	char* m_Buffer;
-	unsigned int m_Size;
-
-public:
-	String(const char* string)
+public:	
+	void Print() const 
 	{
-		m_Size = strlen(string);
-		m_Buffer = new char[m_Size + 1];
-		memcpy(m_Buffer, string, m_Size);
-		m_Buffer[m_Size] = 0;
+		std::cout << "Hell!" << std::endl;
 	}
-
-	String(const String& other)
-		: m_Size(other.m_Size)
-	{
-		std::cout << "Copied String!\n";
-		m_Buffer = new char[m_Size + 1];
-		memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
-	}
-
-	~String()
-	{
-		delete[] m_Buffer;
-	}
-
-	char& operator[](unsigned int index)
-	{
-		return m_Buffer[index];
-	}
-
-	friend std::ostream& operator<<(std::ostream& stream, const String& string);
 };
-
-
-std::ostream& operator<<(std::ostream& stream, const String& string)
-{
-	stream << string.m_Buffer;
-	return stream;
-}
-
-
-void PrintString(const String& string)
-{
-	std::cout << string << std::endl;
-}
 
 
 int main()
 {
-	String string = "Cherno";
-	String second = string;
+	Entity e;
+	e.Print();
 
-	second[2] = 'a';
+	Entity* ptr = &e;
+	ptr->Print(); // or (*ptr).Print();
 
-	PrintString(string);
-	PrintString(second);
+	Entity& entity = *ptr;
+	entity.Print();
+
+
 }

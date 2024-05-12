1e995edae03bf66d5cb0a353c5c9d2ab9abf4e72
Mars-37 <master_sbo@mail.ru>
2024-05-12 12:07:06 +0300

    less 046 Dynamic Arrays (std::vector)

--- main.cpp
+++ main.cpp
@@ -28,4 +28,11 @@ std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
 int main()
 {
 	std::vector<Vertex> vertices;
+
+	vertices.push_back({ 1, 2, 3 });
+	vertices.push_back({ 4, 5, 6 });
+
+	for (int i = 0; i < vertices.size(); ++i) {
+		std::cout << vertices[i] << std::endl;
+	}
 }

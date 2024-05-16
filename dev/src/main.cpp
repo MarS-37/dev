0fd6bf4178012bbf66f7888c51f68a7166eaf8e7
Mars-37 <master_sbo@mail.ru>
2024-05-16 20:15:02 +0300

    Stack vs Heap Memory

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -22,14 +22,16 @@ struct Vector3
 
 int main()
 {
-	int value = 5;
-	int array[5];
-	array[0] = 1;
-	array[1] = 2;
-	array[2] = 3;
-	array[3] = 4;
-	array[4] = 5;
-	Vector3 vector;
+	{
+		int value = 5;
+		int array[5];
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+		array[3] = 4;
+		array[4] = 5;
+		Vector3 vector;
+	}
 
 
 	int* hvalue = new int;

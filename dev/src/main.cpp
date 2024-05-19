3860b4834deb8cb2ed758c71c0fd7705aab72ebc
Mars-37 <master_sbo@mail.ru>
2024-05-19 12:36:47 +0300

    Function Pointers

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -25,7 +25,7 @@ void ForEach(const std::vector<int>& values, void(*func)(int))
 int main()
 {
 	std::vector<int> values = { 1, 5, 4, 2, 3 };
-	ForEach(values, PrintValue);
+	ForEach(values, [](int value) {std::cout << "Value: " << value << std::endl; });
 
 
 	std::cin.get();

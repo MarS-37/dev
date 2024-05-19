b7a9eff103ef69c7d02c038e8b1e47bf09d8c871
Mars-37 <master_sbo@mail.ru>
2024-05-19 21:26:55 +0300

    Lambdas

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -19,6 +19,8 @@ void ForEach(const std::vector<int>& values, const std::function<void(int)>& fun
 int main()
 {
 	std::vector<int> values = { 1, 5, 4, 2, 3 };
+	auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
+	std::cout << *it << std::endl;
 
 	int a = 5;
 

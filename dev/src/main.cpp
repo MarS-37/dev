4240e3690c78656ac775c912c8999292e2fb22f8
Mars-37 <master_sbo@mail.ru>
2024-05-19 12:33:47 +0300

    Function Pointers

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -6,21 +6,26 @@
 
 
 #include <iostream>
+#include <vector>
 
 
-void Hell(int a)
+void PrintValue(int value)
 {
-	std::cout << "meshall " << a << std::endl;
+	std::cout << "Value: " << value << std::endl;
+}
+
+
+void ForEach(const std::vector<int>& values, void(*func)(int))
+{
+	for (int value : values)
+		func(value);
 }
 
 
 int main()
 {
-	typedef void(*HellFunction)(int);
-	HellFunction function = Hell;
-
-	function(8);
-	function(2);
+	std::vector<int> values = { 1, 5, 4, 2, 3 };
+	ForEach(values, PrintValue);
 
 
 	std::cin.get();

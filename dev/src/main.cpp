4df1f41e8ec1889b6142a057bcaf852b14da1c36
Mars-37 <master_sbo@mail.ru>
2024-05-19 11:30:17 +0300

    Static Arrays

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,56 +1,23 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                         The "auto" keyword                       |
+//|                           Static Arrays                          |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
-#include <unordered_map>
+
 #include <iostream>
-#include <string>
-#include <vector>
-
-
-auto GetName()
-{
-	return "Cherno";
-}
-
-class Device {};
-
-
-class DeviceManager
-{
-private:
-	std::unordered_map<std::string, std::vector<Device*>> m_Device;
-
-public:
-	const std::unordered_map<std::string, std::vector<Device*>>& GetDevices() const
-	{
-		return m_Device;
-	}
-};
+#include <array>
 
 
 int main()
 {
-	std::vector<std::string> strings;
-	strings.push_back("Apple");
-	strings.push_back("Orange");
+	std::array<int, 5> data;
+	data[0] = 2;
+	data[4] = 1;
 
 
-	for (auto it = strings.begin();
-		it != strings.end(); ++it)
-	/* or
-	for (std::vector<std::string>::iterator it = strings.begin();
-		it != strings.end(); ++it)*/
-	{
-		std::cout << *it << std::endl;
-	}
-
-
-	DeviceManager dm;
-	const auto& devices = dm.GetDevices();
-	// or const std::unordered_map<std::string, std::vector<Device*>>& devices = dm.GetDevices();
+	std::array<int, 5> dataOld;
+	dataOld[0] = 0;
 
 
 	std::cin.get();

8a9e0665eabd23326ccbffe0aa5b5249e3709cfa
Mars-37 <master_sbo@mail.ru>
2024-05-19 21:48:29 +0300

    using namespace

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,12 +1,13 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                             Lambdas                              |
+//|                           using namespace                        |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
-#include <iostream>
 #include <vector>
+#include <string>
+#include <iostream>
 #include <functional>
 
 void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
@@ -16,8 +17,34 @@ void ForEach(const std::vector<int>& values, const std::function<void(int)>& fun
 }
 
 
+namespace apple {
+	void print(const std::string& text)
+	{
+		std::cout << text << std::endl;
+	}
+}
+
+
+namespace orange {
+	void print(const char* text)
+	{
+		std::string temp = text;
+		std::reverse(temp.begin(), temp.end());
+		std::cout << temp << std::endl;
+	}
+}
+
+
+using namespace apple;
+
 int main()
 {
+
+	print("Hell");
+
+
+#if 0
+
 	std::vector<int> values = { 1, 5, 4, 2, 3 };
 	auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
 	std::cout << *it << std::endl;
@@ -27,6 +54,8 @@ int main()
 	auto lambda = [=](int value) {std::cout << "Value: " << a << std::endl; };
 
 	ForEach(values, lambda);
+
+#endif
 
 
 	std::cin.get();

9b1e757ea9cf131d7565b227cf5585e8d204aa23
Mars-37 <master_sbo@mail.ru>
2024-05-20 11:40:14 +0300

    Threads

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -9,15 +9,23 @@
 #include <thread>
 
 
+static bool s_Finished = false;
+
+
 void DoWork()
 {
-
+	while (!s_Finished) {
+		std::cout << "Working...\n";
+	}
 }
 
 
 int main()
 {
 	std::thread worker(DoWork);
+
+	std::cin.get();
+	s_Finished = true;
 
 	worker.join();
 

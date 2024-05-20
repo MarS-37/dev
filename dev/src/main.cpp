00382bca60ce68157f34ddabbc1d96c91fecbdc4
Mars-37 <master_sbo@mail.ru>
2024-05-20 15:24:22 +0300

    Timing

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,41 +1,25 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                              Threads                             |
+//|                              Timing                              |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
 #include <iostream>
+#include <chrono>
 #include <thread>
-
-
-static bool s_Finished = false;
-
-
-void DoWork()
-{
-	using namespace std::literals::chrono_literals;
-
-
-	std::cout << "Started thread id = " << std::this_thread::get_id() << std::endl;
-
-	while (!s_Finished) {
-		std::cout << "Working...\n";
-		std::this_thread::sleep_for(1s);
-	}
-}
 
 
 int main()
 {
-	std::thread worker(DoWork);
+	using namespace std::literals::chrono_literals;
 
-	std::cin.get();
-	s_Finished = true;
+	auto start = std::chrono::high_resolution_clock::now();
+	std::this_thread::sleep_for(1s);
+	auto end = std::chrono::high_resolution_clock::now();
 
-	worker.join();
-	std::cout << "Finished." << std::endl;
-	std::cout << "Started thread id = " << std::this_thread::get_id() << std::endl;
+	std::chrono::duration<float> duration = end - start;
+	std::cout << duration.count() << "s " << std::endl;
 
 
 	std::cin.get();

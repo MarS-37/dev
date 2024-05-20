90a420def022c519d4a2a8cd95e22a9e6287dfe0
Mars-37 <master_sbo@mail.ru>
2024-05-20 08:27:32 +0300

    Namespaces

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,20 +1,13 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                           using namespace                        |
+//|                             Namespaces                           |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
-#include <vector>
 #include <string>
 #include <iostream>
-#include <functional>
-
-void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
-{
-	for (int value : values)
-		func(value);
-}
+#include <algorithm>
 
 
 namespace apple {
@@ -37,23 +30,7 @@ namespace orange {
 
 int main()
 {
-
 	apple::print("Hell");
-
-
-#if 0
-
-	std::vector<int> values = { 1, 5, 4, 2, 3 };
-	auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
-	std::cout << *it << std::endl;
-
-	int a = 5;
-
-	auto lambda = [=](int value) {std::cout << "Value: " << a << std::endl; };
-
-	ForEach(values, lambda);
-
-#endif
 
 
 	std::cin.get();

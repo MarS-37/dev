2b2fec416f1e72ef763bd4ed44954d0781ef8dc8
Mars-37 <master_sbo@mail.ru>
2024-05-21 21:07:33 +0300

    Multidimensional Arrays

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -16,11 +16,25 @@ int main()
 		a2d[i] = new int[5];
 	}
 
+	for (int y = 0; y < 5; y++) {
+		for (int x = 0; x < 5; x++) {
+			a2d[x][y] = 2;
+		}
+	}
+
 	for (int i = 0; i < 5; i++) {
 		delete[] a2d[i];
 	}
 
 	delete[] a2d;
+
+
+	int* array = new int[5 * 5];
+	for (int y = 0; y < 5 * 5; y++) {
+		for (int x = 0; x < 5; x++) {
+			array[x + y * 5] = 2;
+		}
+	}
 
 
 	std::cin.get();

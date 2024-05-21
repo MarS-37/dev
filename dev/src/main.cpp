cee8ab3d2d9afd296a58abf5cd2343edf5ac95e0
Mars-37 <master_sbo@mail.ru>
2024-05-21 21:36:48 +0300

    Sorting

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -14,7 +14,10 @@
 int main()
 {
 	std::vector<int> values{ 3, 5, 1, 4, 2 };
-	std::sort(values.begin(), values.end(), std::greater<int>());
+	std::sort(values.begin(), values.end(), [](int a, int b)
+		{
+			return a < b;
+		});
 
 	for (int value : values) {
 		std::cout << value << std::endl;

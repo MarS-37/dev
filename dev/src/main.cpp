3c57bb1f24134ab380047ecd3f8dde6c54c3c815
Mars-37 <master_sbo@mail.ru>
2024-05-21 20:28:34 +0300

    Multidimensional Arrays

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,48 +1,18 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                              Timing                              |
+//|                     Multidimensional Arrays                      |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
 #include <iostream>
-#include <chrono>
-#include <thread>
-
-
-struct Timer
-{
-	std::chrono::time_point<std::chrono::steady_clock> start, end;;
-	std::chrono::duration<float> duration;
-
-	Timer()
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-	}
-	~Timer()
-	{
-		end = std::chrono::high_resolution_clock::now();
-		duration = end - start;
-
-		float ms = duration.count() + 1000.0f;
-		std::cout << "Timer took " << ms << "ms\n";
-	}
-};
-
-
-void Function()
-{
-	Timer timer;
-
-	for (int i = 0; i < 100; ++i) {
-		std::cout << "Hell\n";
-	}
-}
 
 
 int main()
 {
-	Function();
+	int* array = new int[50];
+
+	int** a2d = new int* [50];
 
 
 	std::cin.get();

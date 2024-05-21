f9e5d85855b79390628861ad78f9f15286f4dd09
Mars-37 <master_sbo@mail.ru>
2024-05-21 20:54:11 +0300

    Multidimensional Arrays

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -16,19 +16,9 @@ int main()
 		a2d[i] = new int[50];
 	}
 
-
-	int*** a3d = new int** [50];
-
-	for (int i = 0; i < 50; i++) {		
-		a3d[i] = new int* [50];
-
-		for (int j = 0; j < 50; j++) {
-			int** ptr = a3d[i];
-			ptr[j] = new int[50];
-		}
-	}
-
-	a3d[0][0][0] = 0;
+	a2d[0][0] = 0;
+	a2d[0][1] = 0;
+	a2d[0][2] = 0;
 
 
 	std::cin.get();

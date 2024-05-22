94fbb37c3d8ef0f74373b8e3fff5fcbb75054d96
Mars-37 <master_sbo@mail.ru>
2024-05-22 19:15:46 +0300

    Type Punning

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -8,11 +8,19 @@
 #include <iostream>
 
 
+struct Entity
+{
+	int x, y;
+};
+
+
 int main()
 {
-	int a = 50;
-	double& value = *(double*)&a;
-	std::cout << value << std::endl;
+	Entity e = { 5, 8 };
+
+	int* position = (int*)&e;
+
+	std::cout << position[0] << ", " << position[1] << std::endl;
 
 
 	std::cin.get();

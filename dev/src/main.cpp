71af02fdb858fbd1c911ee52bcf79de7c94ee6a1
Mars-37 <master_sbo@mail.ru>
2024-05-22 19:26:32 +0300

    Type Punning

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -11,6 +11,11 @@
 struct Entity
 {
 	int x, y;
+
+	int* GetPositions()
+	{
+		return &x;
+	}
 };
 
 
@@ -18,11 +23,11 @@ int main()
 {
 	Entity e = { 5, 8 };
 
-	int* position = (int*)&e;
+	int* position = e.GetPositions();
 
 	int y = *(int*)((char*)&e + 4);
 
-	std::cout << position[0] << ", " << y << std::endl;
+	std::cout << *position << ", " << y << std::endl;
 
 
 	std::cin.get();

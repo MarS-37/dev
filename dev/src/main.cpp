c61cea888bd52612b12b318fb9dc12aa38ea5760
Mars-37 <master_sbo@mail.ru>
2024-05-25 12:13:36 +0300

    BENCHMARKING

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -43,11 +43,15 @@ int main()
 {
 	int value = 0;
 
-	for (int i = 0; i < 1000000; ++i)
-		value += 2;
+	{
+		Timer timer;
+
+		for (int i = 0; i < 1000000; ++i)
+			value += 2;
+	}
 
 	std::cout << value << std::endl;
-
+	
 
 	__debugbreak();
 }

d337d27738677319134537fc982a70d5843025d5
Mars-37 <master_sbo@mail.ru>
2024-05-27 15:37:08 +0300

    How to Deal with OPTIONAL Data

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -8,28 +8,27 @@
 #include <iostream>
 #include <fstream>
 
+#include <optional>
 
-std::string ReadFileAsString(const std::string& filepath, bool& outSuccess)
+
+std::optional<std::string> ReadFileAsString(const std::string& filepath)
 {
 	std::ifstream stream(filepath);
 	if (stream)	{
 		std::string result;
 		// read file
 		stream.close();
-		outSuccess = true;
 		return result;
 	}
 
-	outSuccess = false;
-	return std::string();	// or return "";
+	return {};
 }
 
 
 int main()
 {
-	bool fileOpenedSuccessfully;
-	std::string data = ReadFileAsString("data.txt", fileOpenedSuccessfully);
-	if (fileOpenedSuccessfully) {
+	auto data = ReadFileAsString("data.txt");	// or std::optional<std::string>
+	if (data.has_value()) {
 
 	}
 

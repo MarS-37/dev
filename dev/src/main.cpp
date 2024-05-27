14528d5e10486a8a600ef09a04612f056cdab772
Mars-37 <master_sbo@mail.ru>
2024-05-27 20:44:02 +0300

    How to make your STRINGS FASTER

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -18,32 +18,36 @@ void* operator new(size_t size)
 	return malloc(size);
 }
 
-
-void PrintName(const std::string& name)
+#if STRING_VIEW
+void PrintName(std::string_view name)
 {
 	std::cout << name << std::endl;
 }
 
+#else
+void PrintName(const std::string& name)
+{
+	std::cout << name << std::endl;
+}
+#endif
 
 int main()
 {
-	const char* name = "Yan Chernikov";
+	std::string name = "Yan Chernikov";
 
-#if 0
+
+#if STRING_VIEW
+	std::string_view firstName(name.c_str(), 3);
+	std::string_view lastName(name.c_str() + 4, 9);
+#else
 	std::string firstName = name.substr(0, 3);
 	std::string lastName = name.substr(4, 9);
-
-#else
-
-	std::string_view firstName(name, 3);
-	std::string_view lastName(name + 4, 9);
-
 #endif
 
-	PrintName("Cherno");
 
-	//PrintName(firstName);
-	//PrintName(lastName);
+	PrintName("Cherno");
+	PrintName(firstName);
+	PrintName(lastName);
 
 
 	std::cout << s_AllocCount << " allocations " << std::endl;

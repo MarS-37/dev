976f3f2cddde1ece5671ffa77b38e84594cce632
Mars-37 <master_sbo@mail.ru>
2024-05-27 17:39:43 +0300

    Multiple TYPES of Data in a SINGLE VARIABLE

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,44 +1,20 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                  How to Deal with OPTIONAL Data                  |
+//|            Multiple TYPES of Data in a SINGLE VARIABLE           |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
 #include <iostream>
-#include <fstream>
 
-#include <optional>
-
-
-std::optional<std::string> ReadFileAsString(const std::string& filepath)
-{
-	std::ifstream stream(filepath);
-	if (stream)	{
-		std::string result;
-		// read file
-		stream.close();
-		return result;
-	}
-
-	return {};
-}
+#include <variant>
 
 
 int main()
 {
-	std::optional<std::string> data = ReadFileAsString("data.txt");	// or auto
-
-	std::string value = data.value_or("Not present");
-	std::cout << value << std::endl;
-
-	if (data.has_value()) {
-		std::cout << "File read successfully!\n";
-	}
-	else {
-		std::cout << "File could not be opened!\n";
-	}
-
+	std::variant<std::string, int> data;
+	data = "Cherno";
+	data = 2;
 
 	std::cin.get();
 }

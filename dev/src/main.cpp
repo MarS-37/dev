7d070344c88b5f0ab0d10ae252d88ea7cce82211
Mars-37 <master_sbo@mail.ru>
2024-05-27 17:55:18 +0300

    Multiple TYPES of Data in a SINGLE VARIABLE

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -15,6 +15,13 @@ int main()
 	std::variant<std::string, int> data;
 	data = "Cherno";
 	std::cout << std::get<std::string>(data) << "\n";
+	if (auto value = std::get_if<std::string>(&data)) {
+		std::string& v = *value;
+	}
+	else {
+
+	}
+
 	data = 2;
 	std::cout << std::get<int>(data) << "\n";
 	

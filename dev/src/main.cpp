d2a5bed2ca862e981fd14ba12a716ad8fbe5e198
Mars-37 <master_sbo@mail.ru>
2024-05-29 06:46:33 +0300

    SINGLETONS

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,191 +1,43 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|     VISUAL BENCHMARKING (how to measure performance visually)    |
+//|                            SINGLETONS                            |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
-#include <algorithm>
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <chrono>
-#include <cmath>
-
-#include <thread>
 
 
-struct ProfileResult
-{
-	std::string Name;
-	long long Start, End;
-	uint32_t ThreadID;
-};
-
-
-struct InstrumentationSession
-{
-	std::string Name;
-};
-
-
-class Instrumentor
-{
-private:
-	InstrumentationSession* m_CurrentSession;
-	std::ofstream m_OutputStream;
-	int m_ProfileCount;
-
-public:
-	Instrumentor()
-		: m_CurrentSession(nullptr), m_ProfileCount(0)
-	{
-
-	}
-
-	void BeginSession(const std::string& name, const std::string& filepath = "results.json")
-	{
-		m_OutputStream.open(filepath);
-		WriteHeader();
-		m_CurrentSession = new InstrumentationSession{ name };
-	}
-
-	void EndSession()
-	{
-		WriteFooter();
-		m_OutputStream.close();
-		delete m_CurrentSession;
-		m_CurrentSession = nullptr;
-		m_ProfileCount = 0;
-	}
-
-	void WriteProfile(const ProfileResult& result)
-	{
-		if (m_ProfileCount++ > 0)
-			m_OutputStream << ",";
-
-		std::string name = result.Name;
-		std::replace(name.begin(), name.end(), '"', '\'');
-
-
-		m_OutputStream << "{";
-		m_OutputStream << "\"cat\":\"function\",";
-		m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-		m_OutputStream << "\"name\":\"" << name << "\",";
-		m_OutputStream << "\"ph\":\"X\",";
-		m_OutputStream << "\"pid\":0,";
-		m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-		m_OutputStream << "\"ts\":" << result.Start;
-		m_OutputStream << "}";
-
-		m_OutputStream.flush();
-	}
-
-	void WriteHeader()
-	{
-		m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-		m_OutputStream.flush();
-	}
-
-	void WriteFooter()
-	{
-		m_OutputStream << "]}";
-		m_OutputStream.flush();
-	}
-
-	static Instrumentor& Get()
-	{
-		static Instrumentor* instance = new Instrumentor();
-		return *instance;
-	}
-};
-
-
-class InstrumentationTimer
+class Singleton
 {
 public:
-	InstrumentationTimer(const char* name)
-		: m_Name(name), m_Stopped(false)
+	Singleton(const Singleton&) = delete;
+
+	static Singleton& Get()
 	{
-		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+		return s_Instance;
 	}
 
-	~InstrumentationTimer()
-	{
-		if (!m_Stopped)
-			Stop();
-	}
-
-	void Stop()
-	{
-		auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-		uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-		Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
-
-		m_Stopped = true;
-	}	
+	void Function() {}
 
 private:
-	const char* m_Name;
-	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
-	bool m_Stopped;
+	Singleton() {}
+
+	float m_Member = 0.0f;
+
+	static Singleton s_Instance;
 };
 
 
-#define PROFILING 1
-#if PROFILING
-#define PROFILE_SCOPE(name) InstrumentationTimer timer##__LINE__(name);
-#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCSIG__)
-#else
-#define PROFILE_SCOPE(name)
-#endif
-
-
-namespace Benchmark {
-	void PrintFunction(int value)
-	{
-		PROFILE_FUNCTION();
-
-		for (int i = 0; i < 1000; ++i) {
-			std::cout << "Hello World # " << i << std::endl;
-		}
-	}
-
-
-	void PrintFunction()
-	{
-		PROFILE_FUNCTION();
-
-		for (int i = 0; i < 1000; ++i) {
-			std::cout << "Hello World # " << sqrt(i) << std::endl;
-		}
-	}
-
-
-	void RunBenchmarks()
-	{
-		PROFILE_FUNCTION();
-
-
-		std::cout << "RunBenchmarks...\n";
-		std::thread a([]() { PrintFunction(2); });
-		std::thread b([]() { PrintFunction(); });
-
-		a.join();
-		b.join();
-	}
-}
+Singleton Singleton::s_Instance;
 
 
 int main()
 {
-	Instrumentor::Get().BeginSession("Profile");
-	Benchmark::RunBenchmarks();
-	Instrumentor::Get().EndSession();
+	auto& instance = Singleton::Get(); // or Singleton& instance = ...
+
+
+	instance.Function();
 
 
 	std::cin.get();

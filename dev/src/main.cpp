e9f44e5ef25db0966b9d24bbd3e7f0388a8f4a7e
Mars-37 <master_sbo@mail.ru>
2024-05-30 18:19:39 +0300

    lvalues and rvalues

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -9,56 +9,16 @@
 #include <memory>
 
 
-struct AllocationMetrics
+int GetValue()
 {
-	uint32_t TotalAllocated = 0;
-	uint32_t TotalFreed = 0;
-
-	uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
-};
-
-
-static AllocationMetrics s_AllocationMetrics;
-
-
-void* operator new(size_t size)
-{
-	s_AllocationMetrics.TotalAllocated += size;
-
-	return malloc(size);
-}
-
-
-void operator delete(void* memory, size_t size)
-{
-	s_AllocationMetrics.TotalFreed += size;
-
-	free(memory);
-}
-
-
-struct Object
-{
-	int x, y, z;
-};
-
-
-static void PrintMemoryUsage()
-{
-	std::cout << "Memory Usage: " << s_AllocationMetrics.CurrentUsage() << " bytes\n";
+	return 10;
 }
 
 
 int main()
 {
-	PrintMemoryUsage();
-	std::string string = "Cherno";
-	PrintMemoryUsage();
-	{
-		std::unique_ptr<Object> obj = std::make_unique<Object>();
-		PrintMemoryUsage();
-	}
-	PrintMemoryUsage();
+	int i = GetValue();
+	GetValue() = 5;
 
 
 	std::cin.get();

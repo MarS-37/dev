2837dd178192366070623d71e1654a17826bca41
Mars-37 <master_sbo@mail.ru>
2024-05-30 18:06:30 +0300

    Track MEMORY ALLOCATIONS the Easy Way

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -9,9 +9,21 @@
 #include <memory>
 
 
+struct AllocationMetrics
+{
+	uint32_t TotalAllocated = 0;
+	uint32_t TotalFreed = 0;
+
+	uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
+};
+
+
+static AllocationMetrics s_AllocationMetrics;
+
+
 void* operator new(size_t size)
 {
-	std::cout << "Allocationg " << size << " bytes\n";
+	s_AllocationMetrics.TotalAllocated += size;
 
 	return malloc(size);
 }
@@ -19,7 +31,7 @@ void* operator new(size_t size)
 
 void operator delete(void* memory, size_t size)
 {
-	std::cout << "Freeing " << size << " byte\n";
+	s_AllocationMetrics.TotalFreed += size;
 
 	free(memory);
 }
@@ -31,13 +43,23 @@ struct Object
 };
 
 
+static void PrintMemoryUsage()
+{
+	std::cout << "Memory Usage: " << s_AllocationMetrics.CurrentUsage() << " bytes\n";
+}
+
+
 int main()
 {
+	PrintMemoryUsage();
+	std::string string = "Cherno";
+	PrintMemoryUsage();
 	{
 		std::unique_ptr<Object> obj = std::make_unique<Object>();
+		PrintMemoryUsage();
 	}
-	{
-		std::string string = "Cherno";
-	}
+	PrintMemoryUsage();
+
+
 	std::cin.get();
 }

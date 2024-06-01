1e17291851ddcc54bff7d895c30a712671db567b
Mars-37 <master_sbo@mail.ru>
2024-06-01 11:26:13 +0300

    Move Semantics

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,24 +1,69 @@
 //+------------------------------------------------------------------+
 //|                                                                  |
-//|                     Argument Evaluation Order                    |
+//|                           Move Semantics                         |
 //|                                                                  |
 //+------------------------------------------------------------------+
 
 
 #include <iostream>
-#include <memory>
 
 
-void printSum(int a, int b)
+class String
 {
-	std::cout << a << " + " << b << " = " << (a + b) << std::endl;
-}
+public:
+	String() = default;
+	String(const char* string)
+	{
+		printf("Created!\n");
+		m_Size = strlen(string);
+		m_Data = new char[m_Size];
+		memcpy(m_Data, string, m_Size);
+	}
+	String(const String& other)
+	{
+		printf("Copied!\n");
+		m_Size = other.m_Size;
+		m_Data = new char[m_Size];
+		memcpy(m_Data, other.m_Data, m_Size);
+	}
+	~String()
+	{
+		delete m_Data;
+	}
+	void Print()
+	{
+		for (uint32_t i = 0; i < m_Size; i++)
+			printf("%C", m_Data[i]);
+
+		printf("\n");
+	}
+
+private:char* m_Data;
+	   uint32_t m_Size;
+};
+
+
+class Entity
+{
+public:
+	Entity(const String& name)
+		:m_Name(name)
+	{
+	}
+	void PrintName()
+	{
+		m_Name.Print();
+	}
+
+private:
+	String m_Name;
+};
 
 
 int main()
 {
-	int value = 0;
-	printSum(++value, ++value);
+	Entity entity("Cherno"); // or Entity entity(String("Cherno"));
+	entity.PrintName();
 
 
 	std::cin.get();
